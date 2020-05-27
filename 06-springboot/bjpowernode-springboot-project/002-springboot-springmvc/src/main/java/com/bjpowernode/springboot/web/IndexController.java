c1741b40b5516338c6568c5c8bfc60b57246f145
freesber <saber.lover@qq.com>
2020-05-27 22:58:14 +0800

    动力节点SpringBoot：01-02

---
+++ 06-springboot/bjpowernode-springboot-project/002-springboot-springmvc/src/main/java/com/bjpowernode/springboot/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/springboot/say")
+    public @ResponseBody String say(){
+        return "Hello,SpringBoot!";
+    }
+
+}
---
+++ 06-springboot/bjpowernode-springboot-project/ReadMe.md
@@ -0,0 +1,3 @@
+## 说明
+
+B站课程，动力节点SpringBoot全套教程[最详细的SpringBoot教程](https://www.bilibili.com/video/BV1PZ4y1j7QK)
